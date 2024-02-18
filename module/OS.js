f3fd9cba65fc133f5559270dc425cd6e2374f411
izzatbekmadaminov7141 <izzatbekmadaminov7141@gmail.com>
2024-02-18 21:15:53 +0500

    ushbu darsda os bilan tanishdik ular platform,cpus,freemem, totalmem, uptime

---
+++ module/OS.js
@@ -0,0 +1,19 @@
+const os = require("os");
+// platform
+// console.log(os.platform());
+
+// qaysi bit ekanligini chiqarib beradi
+// console.log(os.arch());
+
+// umumiy malumot olsih uchun
+// console.log(os.cpus());
+
+// kompyuterda qancha bosh joy bor ekanligini  bilmoqchi bo'lsak
+// console.log(os.freemem()); bit orqaik chiqaradi
+
+// umumiy xotira
+// console.log(os.totalmem()); bit orqalik chiqaradi
+
+// qancha mudatdan beri kompyuterni ishlatayotgani bilish uchun yangilangan vaqtidan
+
+// console.log(os.uptime()); milliy sekunda chiqaradi
