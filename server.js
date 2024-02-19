b20760b8fdaffb5fcb638e6cbd1d75b688f1e796
izzatbekmadaminov7141 <izzatbekmadaminov7141@gmail.com>
2024-02-19 23:15:05 +0500

    server: Javob (Response) yuborish o'rgandik

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
+  //   console.log(req.url);
+  //   console.log(req.method);
+  //   res.end(`<h1 style="text-align:center"> Malumot topilmadi </h1>`);
+  if (req.url === "/") {
+    res.end("malumot yuborildi");
+  } else if (req.url === "/users") {
+    res.end("users yuborildi");
+  } else {
+    res.end("Malumot Topilmadi");
+  }
 });
 
 const POST = process.env.POST || 3000;
