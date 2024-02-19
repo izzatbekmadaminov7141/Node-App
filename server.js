56dd70e8b32bb29895785eff0169b36c1df781d9
izzatbekmadaminov7141 <izzatbekmadaminov7141@gmail.com>
2024-02-19 22:32:18 +0500

    request bo'limi bilan tanishib chiqdik

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const http = require("http");
-http.createServer((req, res) => console.log(req));
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log(req.method);
+});
+
+const POST = process.env.POST || 3000;
+
+server.listen(POST, () => {
+  console.log("listening on");
+});
